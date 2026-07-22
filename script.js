5b558d89a5704d888358daa72dfbb8191e1bbdd2
Drie <drexchris16709@gmail.com>
2026-07-23 01:38:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1028,7 +1028,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     setTimeout(() => {
       // ⚠️ Change this to `true` when your server comes back online
-      const isServerOnline = "true"
+      const isServerOnline = false
 
       if (isServerOnline) {
         spinner.remove();
